2f69058fa8912bb2a7e7448a2d3392c52c5952e6
Pedro Bersch <pedroleottebersch@gmail.com>
2024-07-18 23:59:54 -0300

    removido testes de senvio de email de apllication.class

--- src/main/java/br/com/example/trabalhobackend/TrabalhoBackendApplication.java
+++ src/main/java/br/com/example/trabalhobackend/TrabalhoBackendApplication.java
@@ -1,13 +1,10 @@
 package br.com.example.trabalhobackend;
 
-import br.com.example.trabalhobackend.service.EmailSenderService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @OpenAPIDefinition(servers = {
     @Server(url = "/", description = "Default Server URL")
@@ -15,14 +12,9 @@ import org.springframework.context.event.EventListener;
 @RequiredArgsConstructor
 @SpringBootApplication
 public class TrabalhoBackendApplication {
-    private final EmailSenderService senderService;
 
     public static void main(String[] args) {
         SpringApplication.run(TrabalhoBackendApplication.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void sendMail() {
-        senderService.sendEmail("pedroleottebersch@gmail.com","forgot password","qualquer coisa bori texti \n texte de quabra de linha");
-    }
 }
