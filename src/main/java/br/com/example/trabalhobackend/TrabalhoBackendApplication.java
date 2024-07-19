6792bac6a5d45af49d3ded2bf422a1b20a9ea93c
Pedro Bersch <pedroleottebersch@gmail.com>
2024-07-18 23:44:33 -0300

    email sender prototype

--- src/main/java/br/com/example/trabalhobackend/TrabalhoBackendApplication.java
+++ src/main/java/br/com/example/trabalhobackend/TrabalhoBackendApplication.java
@@ -1,18 +1,28 @@
 package br.com.example.trabalhobackend;
 
+import br.com.example.trabalhobackend.service.EmailSenderService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @OpenAPIDefinition(servers = {
     @Server(url = "/", description = "Default Server URL")
 })
+@RequiredArgsConstructor
 @SpringBootApplication
 public class TrabalhoBackendApplication {
+    private final EmailSenderService senderService;
 
     public static void main(String[] args) {
         SpringApplication.run(TrabalhoBackendApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail() {
+        senderService.sendEmail("pedroleottebersch@gmail.com","forgot password","qualquer coisa bori texti \n texte de quabra de linha");
+    }
 }
---
+++ src/main/java/br/com/example/trabalhobackend/service/EmailSenderService.java
@@ -0,0 +1,27 @@
+package br.com.example.trabalhobackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailSenderService {
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("\n\n\n\n MAIL SEND SUCCESS \n\n\n\n");
+    }
+}
